1cfd34539498612328121f38638cedf26e2c0ed4
Kristian <Kristianoren@yahoo.com>
2021-02-03 11:43:56 +0100

    added functionality to each navigation button

--- 5Minuti-store-frontend/public_html/JS/javaScript.js
+++ 5Minuti-store-frontend/public_html/JS/javaScript.js
@@ -5,3 +5,29 @@
  */
 
 
+function hideAllMenu(){
+    document.getElementById('home').style.display ='none';
+    document.getElementById('contact').style.display ='none';
+    document.getElementById('logIn').style.display ='none';
+    document.getElementById('menu').style.display ='none';
+}
+
+function showMenu(){
+    hideAllMenu();
+    document.getElementById('menu').style.display ='block';
+}
+
+function showContact(){
+    hideAllMenu();
+    document.getElementById('contact').style.display ='block';
+}
+
+function showHome(){
+    hideAllMenu();
+    document.getElementById('home').style.display ='block';
+}
+
+function showLogIn(){
+    hideAllMenu();
+    document.getElementById('logIn').style.display ='block';
+}
