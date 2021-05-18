3b340924705ee8882a8d59bd9719022e31153191
stigus96 <stighelgebruset@msn.com>
2021-05-18 14:12:02 +0200

    Cleaned code and headers and added sources for some of the classes

--- 5Minuti-store-backend/src/main/java/no/five/min/authentication/JwtUserDetailsService.java
+++ 5Minuti-store-backend/src/main/java/no/five/min/authentication/JwtUserDetailsService.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package no.five.min.authentication;
+
+//code source from https://www.javainuse.com/spring/boot-jwt
 
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -27,10 +21,7 @@ public class JwtUserDetailsService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO - this should be handled in another way. Either stored in a database, or in a config file, but not
-		// In the source code
-                
-                
+               
 		if (userid.equals(username)) {
 			return new User(userid, password,
 					new ArrayList<>());
--- 5Minuti-store-backend/src/main/java/no/five/min/repository/ProductRepository.java
+++ 5Minuti-store-backend/src/main/java/no/five/min/repository/ProductRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 import no.five.min.entity.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     
