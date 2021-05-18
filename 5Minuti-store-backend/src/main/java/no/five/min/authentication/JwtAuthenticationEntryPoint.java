8dafe0ab1778fe89b43aa4c3c7f67c58d4e2e85d
stigus96 <stighelgebruset@msn.com>
2021-05-18 14:17:16 +0200

    adding commits that didn't get added in last commit

--- 5Minuti-store-backend/src/main/java/no/five/min/authentication/JwtAuthenticationEntryPoint.java
+++ 5Minuti-store-backend/src/main/java/no/five/min/authentication/JwtAuthenticationEntryPoint.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package no.five.min.authentication;
+
+//code source from https://www.javainuse.com/spring/boot-jwt
 
 import java.io.IOException;
 import java.io.Serializable;
